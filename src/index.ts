4a10454e793567c097ea232b0fb14f6b6eeaca34
Your Name <you@example.com>
2019-10-14 19:20:10 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@
 
 
 // 錢錢大於一個值，叫乾爹
-let money = 40000;
-let limit = 500000; // 限制值
+//let money = 40000;
+//let limit = 500000; // 限制值
+
+let a : string = '@&*$#';
+
+console.log(a.codePointAt(4));
+
+let b : string = 'love';
+console.log(b.toLocaleLowerCase;
+
+let d : number = 2.6742;
+console.log(d.toPrecision(2));
+
+let f : number = 12.1234;
+console.log(f.toPrecision);
+
+
+
+// 錢錢大於一個值，叫乾爹
+
+
+let money : number = 4000000;
+let limit : number = 500000;
+if(money > limit) {
+    console.log('叫乾爹');
+}else{
+    console.log('You are fire');
+}
 
